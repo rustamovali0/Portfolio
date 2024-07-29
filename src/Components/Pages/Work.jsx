f27580537ce7cab91a8df06aab7e5303f90678f2
Ali Rustamov <125809215+rustamovali0@users.noreply.github.com>
2024-07-29 15:15:31 +0400

    Update Work.jsx

--- src/Components/Pages/Work.jsx
+++ src/Components/Pages/Work.jsx
@@ -11,7 +11,7 @@ const Work = () => {
     <div className="Work">
       <div className="WorkCards">
         <div className="WorkCard">
-          <img src="https://senior.az/master%20academy_files/seniorlogohr.svg" className="img1" alt="" />
+          <img src="https://scontent.fgyd4-2.fna.fbcdn.net/v/t39.30808-1/342764179_1188160978565082_4311906518468152017_n.jpg?stp=c62.0.200.200a_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=hvAFyg4EYbYQ7kNvgGRDVE6&_nc_ht=scontent.fgyd4-2.fna&oh=00_AYD4JhvtSgYuFUQt7x3mdorNvNzYS555inQ12oZIwzR7MA&oe=66AD33BF" className="img1" alt="" />
           <h1>{t('work.senior.title')}</h1>
           <h4>March 2023 - 2024</h4>
           <h3>{t('work.senior.description')}</h3>
