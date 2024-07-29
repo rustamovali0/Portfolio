f9ae57bf07fbc5d2dfb5b260695cee7fdcdcadb3
Ali Rustamov <125809215+rustamovali0@users.noreply.github.com>
2024-07-29 15:13:27 +0400

    Update Experience.jsx

--- src/Components/Pages/Experience.jsx
+++ src/Components/Pages/Experience.jsx
@@ -17,7 +17,7 @@ const Experience = () => {
         <h3>{t('experience.description')}</h3>
       </article>
       <img 
-        src="https://senior.az/master%20academy_files/seniorlogohr.svg" 
+        src="https://scontent.fgyd4-2.fna.fbcdn.net/v/t39.30808-1/342764179_1188160978565082_4311906518468152017_n.jpg?stp=c62.0.200.200a_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=hvAFyg4EYbYQ7kNvgGRDVE6&_nc_ht=scontent.fgyd4-2.fna&oh=00_AYD4JhvtSgYuFUQt7x3mdorNvNzYS555inQ12oZIwzR7MA&oe=66AD33BF" 
         alt="Senior Logo" 
         onClick={openWebsite}
       />
